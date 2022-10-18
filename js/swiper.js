c3cd771ab29f96343facd7c9033fefdbfb079e62
Natallya <natasupernice@gmail.com>
2022-10-19 01:22:09 +0300

    add new swiper slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper__slider', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -43,6 +43,44 @@ const swiper = new Swiper('.swiper', {
     // when window width is >= 640px
     1000: {
       slidesPerView: 3,
+      spaceBetween: 30
+    }
+  },
+});
+
+new Swiper('.swiper__reviews', {
+  direction: 'horizontal',
+  loop: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  slidesPerView: 4,
+
+  // Default parameters
+  spaceBetween: 30,
+// Responsive breakpoints
+  breakpoints: {
+    220: {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    // when window width is >= 320px
+    320: {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    // when window width is >= 480px
+    640: {
+      direction: 'horizontal',
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1000: {
+      slidesPerView: 4,
       spaceBetween: 30
     }
   },
