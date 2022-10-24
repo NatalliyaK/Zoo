5b6bf2b0c0ef7cbe450adb73ffa24ea65164d61b
Natallya <natasupernice@gmail.com>
2022-10-25 02:04:11 +0300

    add burger menu

--- js/index.js
+++ js/index.js
@@ -4,11 +4,13 @@ const donateClick = document.querySelector('.donate-click'),
     slider = document.querySelector('.slider'),
     friend = document.querySelector('.friend'),
     reviews = document.querySelector('.reviews'),
-    donateSection = document.querySelector('.donate');
+    donateSection = document.querySelector('.donate'),
+    headerBurger = document.querySelector('.header__burger'),
+    headerMob = document.querySelector('.header__mob');
 
 donateClick.addEventListener('click',  show);
 aboutClick.addEventListener('click', show);
-
+headerBurger.addEventListener('click', showBurger);
 
 function show() {
     donateSection.classList.toggle('show');
@@ -18,4 +20,11 @@ function show() {
     friend.classList.toggle('hide');
     reviews.classList.toggle('hide');
 }
+
+function showBurger() {
+    headerMob.classList.toggle('active');
+    headerMob.classList.remove('hide');
+    headerBurger.classList.toggle('open');
+}
+
 
